b8ca55fd8e6f96b75147d584a973b718b3cc351f
Md Mahbub Islam <atthemahbub@gmail.com>
2026-01-04 12:02:28 +0600

    Folder structure has been created and Plugin details has been added.

---
+++ modular-elementor-toolkit.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Modular Elementor Toolkit
+ * Description: A modular collection of custom Elementor Widgets.
+ * Version: 1.0.0
+ * Author: Your Name
+ * Text Domain: modular-elementor-toolkit
+ * Requires Plugins: elementor
+ */
+
+if ( ! defined( "ABSPATH" ) ) {
+  exit;
+}
